b04982476932e0eac033a18c0ba48524d298a930
Matt Fonner <matt@matthewfonner.com>
2020-06-09 20:33:12 -0400

    Updated distrib_keys.sh

--- bin/distrib_keys.sh
+++ bin/distrib_keys.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-for ip in `cat /home/matt/ansible/inventory/list_of_servers`; do
-    ssh-copy-id -i ~/.ssh/id_rsa.pub $ip
+# copies ssh keys given a list of servers
+for ip in `cat /path/to/list_of_servers`; do
+    ssh-copy-id -i ~/.ssh/id_ecdsa.pub $ip
 done
